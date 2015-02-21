3dcfb7fbd2aaa8fe0e9549b1b2e8a01d39ea8b0c
Murakami Arisa <arisa@wodin.net>
2015-02-21 15:03:34 +0900

    alice commit

--- count/ViewController.h
+++ count/ViewController.h
@@ -8,8 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    
+    int number;
+    
+    IBOutlet UILabel *label;
+}
 
+-(IBAction)plus;
 
 @end
 
